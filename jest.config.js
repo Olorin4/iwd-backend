3c1815e2489fd47c549f62c6736ca038a34264a3
Nick Kalas <nick.kalas@proton.me>
2025-02-26 09:47:44 +0200

    Fix Jest environment by installing jest-environment-jsdom

--- jest.config.js
+++ jest.config.js
@@ -18,7 +18,7 @@ export default {
     projects: [
         {
             displayName: "frontend",
-            testEnvironment: "jsdom",
+            testEnvironment: "est-environment-jsdom",
             testMatch: ["<rootDir>/frontend/**/*.test.js"], // Matches frontend test files
             moduleNameMapper: {
                 "\\.(css|scss)$": "identity-obj-proxy", // Mock CSS modules in frontend
