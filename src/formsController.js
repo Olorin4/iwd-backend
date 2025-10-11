f3ba1f7aa2f9a252eed16677305e522d221a4eca
Nick Kalas <nick.kalas@proton.me>
2025-10-11 16:24:17 +0300

    debug(forms): add logging for validation errors

--- src/formsController.js
+++ src/formsController.js
@@ -38,7 +38,10 @@ const sanitizeContactForm = [
 
 async function signUpForm(req, res) {
     const errors = validationResult(req);
-    if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
+    if (!errors.isEmpty()) {
+        console.error("Validation errors:", errors.array());
+        return res.status(400).json({ errors: errors.array() });
+    }
 
     console.log("📩 Received Form Data:", req.body);
 
